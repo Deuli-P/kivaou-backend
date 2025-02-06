659110504a1c2789cdbd6e3b36d8d88f9809679c
Deuli-P <pierre.antoniutti@gmail.com>
2025-02-06 11:18:33 +0100

    start login

--- controllers/loginControllers.js
+++ controllers/loginControllers.js
@@ -1,8 +1,25 @@
+import jwt from 'jsonwebtoken';
 
-
-export const getSession = async (req, res) => {
-}:
+// J'ai besoin : 
+// - d'envoyer la demande de connexion avec email et password et de renvoyer si correcte un token
 
 
 export const postLogin = async (req, res) => {
+
+    const { email , password } = req.body;
+
+    if(email=== '' || password === ''){
+        res.status(400).json({message: 'Veuillez remplir tous les champs'});
+    }
+
+    const regetEmail = new RegExp('^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$');
+
+    if(!regetEmail.test(email)){
+        res.status(400).json({message: 'Email incorrect'});
+    }
+    if(password.length < 6){
+        res.status(400).json({message: 'Mot de passe trop court'});
+    }
+
+
 };
--- middlewares.js
+++ middlewares.js
@@ -1,12 +1,60 @@
 
-
+// - J'ai besoin de vérifier si le token est présent et valide
+// - J'ai besoin de savoir si il y a un utilisateur connecté et si il est Admin
 
 export const authMiddleware = async (req, res, next) => {
-    console.log('authMiddleware');
+
+    const token = req.session.token;
+    if(!token){
+        res.status(401).json({message: 'Vous n\'êtes pas connecté'});
+        res.redirect('/login');
+    }
+
+    // Vérifier si le token est valide
+    const verifyToken = jwt.verify(token, process.env.JWT_SECRET,(err, decoded) => {
+        
+        if(err){
+            res.status(401).json({message: 'Token invalide'});
+            return res.redirect('/login');
+        }
+        return decoded;
+    });
+
+    if(!verifyToken){
+        res.status(401).json({message: 'Token invalide'});
+        res.redirect('/login');
+    }
+
     next();
 }
 
 export const adminMiddleware = async (req, res, next) => {
-    console.log('adminMiddleware');
+
+    const token = req.session.token;
+    if(!token){
+        res.status(401).json({message: 'Vous n\'êtes pas connecté'});
+        res.redirect('/login');
+    }
+
+    // Vérifier si le token est valide
+    const verifyToken = jwt.verify(token, process.env.JWT_SECRET,(err, decoded) => {
+        
+        if(err){
+            res.status(401).json({message: 'Token invalide'});
+            return res.redirect('/login');
+        }
+        return decoded;
+    });
+
+    if(!verifyToken){
+        res.status(401).json({message: 'Token invalide'});
+        res.redirect('/login');
+    }
+
+    if(verifyToken.role !== 'admin'){
+        res.status(403).json({message: 'Vous n\'avez pas les droits pour accéder à cette page'});
+        res.redirect('/login');
+    }
+    
     next();
 }
