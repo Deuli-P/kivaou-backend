f0619b67710287a5bd51719599cb20b74773468d
Pierre <pierre.antoniutti@deliverycar.fr>
2025-05-24 19:30:57 +0200

    correction de la table organization

--- migrations/tables/organizations.sql
+++ migrations/tables/organizations.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS organizations (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name TEXT UNIQUE NOT NULL,
-    owner_id UUID NOT NULL,
+    owner_id UUID,
     address_id UUID,
     created_by UUID NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
